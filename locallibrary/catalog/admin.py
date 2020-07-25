c50280f812d7128bb6de79bb13ad69c6e3cd27e7
anish kulung <anishkulung123@gmail.com>
2020-07-25 23:18:17 +0545

    customized admin panels

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,7 +2,37 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
+class BookInstanceInline(admin.TabularInline):
+	model = BookInstance
+
+class BookInline(admin.TabularInline):
+	model  = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+	inlines = [BookInline]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'display_genre')
+	inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	list_filter = ('status', 'due_back')
+	list_display = ('id', 'status', 'due_back')
+	
+	fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
